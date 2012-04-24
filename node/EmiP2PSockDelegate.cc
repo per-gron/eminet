7e88a14ea06ba97d04a79b37dbb3dd865d175eb4
Per Eckerdal <per.eckerdal@gmail.com>
2012-04-24 16:56:45 +0200

    Add EmiP2PSockDelegate (oops.. it's a mistake that it's not in the repo already)

---
+++ node/EmiP2PSockDelegate.cc
@@ -0,0 +1,76 @@
+#define BUILDING_NODE_EXTENSION
+
+#include "EmiP2PSockDelegate.h"
+
+#include "EmiNodeUtil.h"
+#include "EmiP2PSocket.h"
+#include "EmiConnection.h"
+
+#include "../core/EmiP2PSock.h"
+#include <node.h>
+
+using namespace v8;
+
+static void recv_cb(uv_udp_t *socket,
+                    const struct sockaddr_storage& addr,
+                    ssize_t nread,
+                    const v8::Local<v8::Object>& slab,
+                    size_t offset) {
+    HandleScope scope;
+    
+    EmiP2PSocket *wrap = reinterpret_cast<EmiP2PSocket *>(socket->data);
+    
+    if (nread < 0) {
+        const unsigned argc = 2;
+        Handle<Value> argv[argc] = {
+            wrap->handle_,
+            EmiNodeUtil::errStr(uv_last_error(uv_default_loop()))
+        };
+        EmiP2PSocket::connectionError->Call(Context::GetCurrent()->Global(), argc, argv);
+        return;
+    }
+    if (nread > 0) {
+        wrap->getP2PSock().onMessage(EmiConnection::Now(),
+                                     socket,
+                                     addr,
+                                     slab,
+                                     offset,
+                                     nread);
+    }
+}
+
+EmiP2PSockDelegate::EmiP2PSockDelegate(EmiP2PSocket& es) : _es(es) {}
+
+void EmiP2PSockDelegate::closeSocket(EmiP2PSockDelegate::EPS& sock, uv_udp_t *socket) {
+    // This allows V8's GC to reclaim the EmiSocket when no UDP sockets are open
+    //
+    // TODO Perhaps I should do this on the next uv tick, since this might dealloc
+    // the whole socket, which will probably not end up well.
+    //
+    // TODO What happens when this method is actually called from _es's destructor?
+    sock.getDelegate()._es.Unref();
+    
+    EmiNodeUtil::closeSocket(socket);
+}
+
+uv_udp_t *EmiP2PSockDelegate::openSocket(uint16_t port, Error& error) {
+    EPS& sock(_es._sock);
+    
+    uv_udp_t *ret(EmiNodeUtil::openSocket(sock.config.address, port, recv_cb, error));
+    
+    if (ret) {
+        ret->data = &sock.getDelegate()._es;
+        
+        // This prevents V8's GC to reclaim the EmiSocket while UDP sockets are open
+        sock.getDelegate()._es.Ref();
+    }
+    
+    return ret;
+}
+
+void EmiP2PSockDelegate::sendData(uv_udp_t *socket,
+                               const Address& address,
+                               const uint8_t *data,
+                               size_t size) {
+    EmiNodeUtil::sendData(socket, address, data, size);
+}
---
+++ node/EmiP2PSockDelegate.h
@@ -0,0 +1,41 @@
+#define BUILDING_NODE_EXTENSION
+#ifndef emilir_EmiP2PSockDelegate_h
+#define emilir_EmiP2PSockDelegate_h
+
+#include "EmiError.h"
+#include "EmiBinding.h"
+
+#include "../core/EmiTypes.h"
+#include <node.h>
+#include <node_buffer.h>
+#include <uv.h>
+
+class EmiP2PSocket;
+class EmiP2PSockDelegate;
+template<class P2PSockDelegate>
+class EmiP2PSock;
+
+class EmiP2PSockDelegate {
+    typedef EmiP2PSock<EmiP2PSockDelegate> EPS;
+    
+    typedef EmiBinding::Error   Error;
+    typedef EmiBinding::Address Address;
+    
+    EmiP2PSocket& _es;
+public:
+    
+    typedef EmiBinding                 Binding;
+    typedef v8::Persistent<v8::Object> ConnectionOpenedCallbackCookie;
+    
+    EmiP2PSockDelegate(EmiP2PSocket& es);
+    
+    static void closeSocket(EPS& sock, uv_udp_t *socket);
+    uv_udp_t *openSocket(uint16_t port, Error& err);
+    
+    static void sendData(uv_udp_t *socket,
+                         const Address& address,
+                         const uint8_t *data,
+                         size_t size);
+};
+
+#endif
