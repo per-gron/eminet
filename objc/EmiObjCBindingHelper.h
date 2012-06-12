ea3b144fd695930f2607616d33659618f0a69796
Per Eckerdal <per.eckerdal@gmail.com>
2012-06-12 16:27:50 +0200

    Moved some copy-pasted code into separate EmiObjCBindingHelper.h file

--- objc/EmiObjCBindingHelper.h
+++ objc/EmiObjCBindingHelper.h
@@ -9,6 +9,24 @@
 #ifndef rock_EmiObjCBindingHelper_h
 #define rock_EmiObjCBindingHelper_h
 
+#define DISPATCH_SYNC_OR_ASYNC(queue, block, method)        \
+    {                                                       \
+        dispatch_block_t DISPATCH_SYNC__block = (block);    \
+        dispatch_queue_t DISPATCH_SYNC__queue = (queue);    \
+        if (dispatch_get_current_queue() ==                 \
+            DISPATCH_SYNC__queue) {                         \
+            DISPATCH_SYNC__block();                         \
+        }                                                   \
+        else {                                              \
+            dispatch_##method(DISPATCH_SYNC__queue,         \
+                              DISPATCH_SYNC__block);        \
+        }                                                   \
+    }
 
+#define DISPATCH_SYNC(queue, block)                         \
+    DISPATCH_SYNC_OR_ASYNC(queue, block, sync)
+
+#define DISPATCH_ASYNC(queue, block)                        \
+    DISPATCH_SYNC_OR_ASYNC(queue, block, async)
 
 #endif
--- objc/EmiSocket.mm
+++ objc/EmiSocket.mm
@@ -13,30 +13,11 @@
 #import "EmiSocketUserDataWrapper.h"
 #include "EmiSendQueue.h"
 #include "EmiConnDelegate.h"
+#include "EmiObjCBindingHelper.h"
 
 #import "GCDAsyncUdpSocket.h"
 #include <map>
 #include <set>
-
-#define DISPATCH_SYNC_OR_ASYNC(queue, block, method)        \
-    {                                                       \
-        dispatch_block_t DISPATCH_SYNC__block = (block);    \
-        dispatch_queue_t DISPATCH_SYNC__queue = (queue);    \
-        if (dispatch_get_current_queue() ==                 \
-            DISPATCH_SYNC__queue) {                         \
-            DISPATCH_SYNC__block();                         \
-        }                                                   \
-        else {                                              \
-            dispatch_##method(DISPATCH_SYNC__queue,         \
-                              DISPATCH_SYNC__block);        \
-        }                                                   \
-    }
-
-#define DISPATCH_SYNC(queue, block)                         \
-    DISPATCH_SYNC_OR_ASYNC(queue, block, sync)
-
-#define DISPATCH_ASYNC(queue, block)                        \
-    DISPATCH_SYNC_OR_ASYNC(queue, block, async)
 
 #pragma mark - Helper classes
 
