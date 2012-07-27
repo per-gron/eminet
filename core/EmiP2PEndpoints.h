per-gron/eminet

commit ee6cd6dba3fae4e6eee2f4469f9f49640785eefb

Moved code that extracts addresses from EmiP2PEndpoints data from EmiLogicalConnection::gotPrxRstSynAck into reusable methods in EmiP2PEndpoints

core/EmiP2PEndpoints.h
@@ -19,13 +19,16 @@
 class EmiP2PEndpoints {
 public:
     EmiP2PEndpoints() :
+    family(0),
     myEndpointPair(NULL),
     myEndpointPairLength(0),
     peerEndpointPair(NULL),
     peerEndpointPairLength(0) {}
     
-    EmiP2PEndpoints(const uint8_t *myEndpointPair_, size_t myEndpointPairLength_,
+    EmiP2PEndpoints(int family,
+                    const uint8_t *myEndpointPair_, size_t myEndpointPairLength_,
                     const uint8_t *peerEndpointPair_, size_t peerEndpointPairLength_) :
+    family(family),
     myEndpointPair(myEndpointPair_ ? (uint8_t *)malloc(myEndpointPairLength_) : NULL),
     myEndpointPairLength(myEndpointPairLength_),
     peerEndpointPair(peerEndpointPair_ ? (uint8_t *)malloc(peerEndpointPairLength_) : NULL),
@@ -48,6 +51,7 @@ public:
     }
     
     EmiP2PEndpoints(const EmiP2PEndpoints& other) :
+    family(other.family),
     myEndpointPair(other.myEndpointPair ? (uint8_t *)malloc(other.myEndpointPairLength) : NULL),
     myEndpointPairLength(other.myEndpointPairLength),
     peerEndpointPair(other.peerEndpointPair ? (uint8_t *)malloc(other.peerEndpointPairLength) : NULL),
@@ -83,10 +87,44 @@ public:
         return *this;
     }
     
+    const int family;
     uint8_t *myEndpointPair;
     size_t myEndpointPairLength;
     uint8_t *peerEndpointPair;
     size_t peerEndpointPairLength;
+    
+    void extractAddress(bool me, bool inner, sockaddr_storage *addr) {
+        
+        const size_t ipLen = EmiNetUtil::familyIpLength(family);
+        static const size_t portLen = sizeof(uint16_t);
+        const size_t endpointPairLen = 2*(ipLen+portLen);
+        
+        ASSERT(endpointPairLen == (me ? myEndpointPairLength : peerEndpointPairLength));
+        
+        const uint8_t *dataPtr = (me ? myEndpointPair : peerEndpointPair)+(inner ? 0 : ipLen+portLen);
+        
+        EmiNetUtil::makeAddress(family,
+                                dataPtr, ipLen,
+                                *((uint16_t *)(dataPtr+ipLen)),
+                                addr);
+        
+    }
+    
+    void extractMyInnerAddress(sockaddr_storage *addr) {
+        extractAddress(/*me:*/true, /*inner:*/true, addr);
+    }
+    
+    void extractMyOuterAddress(sockaddr_storage *addr) {
+        extractAddress(/*me:*/true, /*inner:*/false, addr);
+    }
+    
+    void extractPeerInnerAddress(sockaddr_storage *addr) {
+        extractAddress(/*me:*/false, /*inner:*/true, addr);
+    }
+    
+    void extractPeerOuterAddress(sockaddr_storage *addr) {
+        extractAddress(/*me:*/false, /*inner:*/false, addr);
+    }
 };
 
 #endif

core/EmiLogicalConnection.h
@@ -294,18 +294,6 @@ public:
             return;
         }
         
-        sockaddr_storage addrs[2];
-        
-        const uint8_t *dataPtr = data+endpointPairLen;
-        for (int i=0; i<2; i++) {
-            EmiNetUtil::makeAddress(family,
-                                    dataPtr, ipLen,
-                                    *((uint16_t *)(dataPtr+ipLen)),
-                                    &addrs[i]);
-            
-            dataPtr += ipLen+portLen;
-        }
-        
         
         /// Release the reliable PRX-ACK handshake message
         
@@ -315,8 +303,14 @@ public:
         /// Initiate NAT punch through
         
         if (!_natPunchthrough) {
-            _p2pEndpoints = EmiP2PEndpoints(/*myEndpointPair:*/data, endpointPairLen,
+            _p2pEndpoints = EmiP2PEndpoints(family,
+                                            /*myEndpointPair:*/data, endpointPairLen,
                                             /*peerEndpointPair:*/data+endpointPairLen, endpointPairLen);
+            
+            sockaddr_storage peerInnerAddr;
+            sockaddr_storage peerOuterAddr;
+            _p2pEndpoints.extractPeerInnerAddress(&peerInnerAddr);
+            _p2pEndpoints.extractPeerOuterAddress(&peerOuterAddr);
             
             _natPunchthrough = new ENP(_conn->config.connectionTimeout,
                                        *this,
@@ -325,8 +319,8 @@ public:
                                        _conn->getRemoteAddress(),
                                        _conn->getP2PData(),
                                        _p2pEndpoints,
-                                       /*peerInnerAddr:*/addrs[0],
-                                       /*peerOuterAddr:*/addrs[1]);
+                                       peerInnerAddr,
+                                       peerOuterAddr);
         }
     }
     

commit 307c7503d9cedff8d7f808391449124409703da6

Fixed stupid bug introduced in ee6cd6dba3fae4e6eee2f4469f9f49640785eefb

core/EmiP2PEndpoints.h
@@ -25,10 +25,10 @@ public:
     peerEndpointPair(NULL),
     peerEndpointPairLength(0) {}
     
-    EmiP2PEndpoints(int family,
+    EmiP2PEndpoints(int family_,
                     const uint8_t *myEndpointPair_, size_t myEndpointPairLength_,
                     const uint8_t *peerEndpointPair_, size_t peerEndpointPairLength_) :
-    family(family),
+    family(family_),
     myEndpointPair(myEndpointPair_ ? (uint8_t *)malloc(myEndpointPairLength_) : NULL),
     myEndpointPairLength(myEndpointPairLength_),
     peerEndpointPair(peerEndpointPair_ ? (uint8_t *)malloc(peerEndpointPairLength_) : NULL),
@@ -72,6 +72,8 @@ public:
             free(peerEndpointPair);
         }
         
+        family = other.family;
+        
         myEndpointPairLength = other.myEndpointPairLength;
         myEndpointPair = other.myEndpointPair ? (uint8_t *)malloc(other.myEndpointPairLength) : NULL;
         if (myEndpointPair) {
@@ -87,7 +89,7 @@ public:
         return *this;
     }
     
-    const int family;
+    int family;
     uint8_t *myEndpointPair;
     size_t myEndpointPairLength;
     uint8_t *peerEndpointPair;