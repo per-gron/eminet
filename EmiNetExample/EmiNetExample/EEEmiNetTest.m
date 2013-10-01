2f45039294550cf97b5e6a94564bb8d554ffcabd
Per Eckerdal <per.eckerdal@gmail.com>
2013-10-01 23:02:31 +0200

    Enhanced EmiNetExample application
    It now properly opens a server/client connection
    and bounces messages between the endpoints.

--- EmiNetExample/EmiNetExample/EEEmiNetTest.m
+++ EmiNetExample/EmiNetExample/EEEmiNetTest.m
@@ -6,7 +6,11 @@
 
 @end
 
-@implementation EEEmiNetTest
+@implementation EEEmiNetTest {
+    NSMutableArray *_uncertainServerConnections;
+    EmiConnection *_serverConnection;
+    EmiConnection *_clientConnection;
+}
 
 - (void)runOnPort:(uint16_t)port
 {
@@ -38,12 +42,22 @@
 
 - (void)emiSocket:(EmiSocket *)socket gotConnection:(EmiConnection *)connection
 {
-    NSLog(@"GOT SOCKET!");
+    NSLog(@"Server connection opened");
+    if (_serverConnection)
+        return;
+    
+    [_uncertainServerConnections addObject:connection];
+    [connection setDelegate:self delegateQueue:dispatch_get_main_queue()];
 }
 
 - (void)emiConnectionOpened:(EmiConnection *)connection userData:(id)userData
 {
-    NSLog(@"Connection opened");
+    NSLog(@"Client connection opened");
+    
+    _clientConnection = connection;
+    NSError *error;
+    [_clientConnection send:[@"hello" dataUsingEncoding:NSUTF8StringEncoding]
+                      error:&error];
 }
 
 - (void)emiConnectionFailedToConnect:(EmiSocket *)socket error:(NSError *)error userData:(id)userData
@@ -54,13 +68,27 @@
 - (void)emiConnectionDisconnect:(EmiConnection *)connection forReason:(EmiDisconnectReason)reason
 {
     NSLog(@"Disconnect");
+    
+    [_uncertainServerConnections removeObject:connection];
+    if (_serverConnection == connection) {
+        _serverConnection = nil;
+        return;
+    }
 }
 
 - (void)emiConnectionMessage:(EmiConnection *)connection
             channelQualifier:(EmiChannelQualifier)channelQualifier
                         data:(NSData *)data
 {
-    NSLog(@"Got message");
+    if ([_uncertainServerConnections containsObject:connection] && !_serverConnection) {
+        [_uncertainServerConnections removeAllObjects];
+        _serverConnection = connection;
+    }
+    
+    NSLog(@"Got message (fromClient %d)", connection == _clientConnection);
+    NSError *error;
+    [connection send:[@"hello" dataUsingEncoding:NSUTF8StringEncoding]
+               error:&error];
 }
 
 @end
